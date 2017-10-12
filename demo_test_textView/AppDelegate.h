6fbd8c66b6f131e69ba54998ec3c4e8921b57ad3
chen liang <chenliang@chendeiMac.local>
2017-10-12 14:34:03 +0800

    Initial Commit

---
+++ demo_test_textView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_test_textView
+//
+//  Created by chen liang on 2017/10/12.
+//  Copyright © 2017年 chen liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
